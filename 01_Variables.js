aaf3dbe4ba5455799dbd011fe384993694874642
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2024-04-05 18:21:39 +0000

    New and old Renamed

---
+++ 01_Variables.js
@@ -0,0 +1,6 @@
+const accoundId = 2145;
+let naming  = "Shivansh";
+var address = "kanpur";
+department = "CSE";
+console.log([accoundId,naming,address,department]); /* New type of printing if we have to print many things at a time */
+
