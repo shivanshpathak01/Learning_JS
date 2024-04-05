7b8df7fe5dffb59b7961c7373d19088652d876e9
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2024-04-05 18:35:03 +0000

    Datatypes

---
+++ 02_Datatype.js
@@ -0,0 +1,3 @@
+"use strict" /* if we write this in any file then the file is used as newer version of javascript */
+console.log(typeof null); // It says that null is an object
+console.log(typeof undefined); // it is being given a special type undefined  
