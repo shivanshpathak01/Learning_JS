fa0ef8005c14ec893e72416c1568cd247eaacc79
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2024-04-08 18:03:17 +0000

    Date commit

---
+++ Basics/07_DatesInJS.js
@@ -0,0 +1,8 @@
+let myCreatedDate = new Date("01-14-2024");
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+/***********************  Time  ********************* */
+
+
