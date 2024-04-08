7740a9835164bd1aba09fbe98b3fdf8e70a7d526
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2024-04-08 18:41:54 +0000

    Sorting lexicographically

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,11 @@
+let myArr=[1 ,5 ,8 ,9 ,10];
+// console.log(myArr);
+// console.log(myArr.fill(5));
+// console.log(myArr.shift());
+// console.log(myArr.shift(10));
+// console.log(myArr.slice(0,2));   --->>>> Array doesn't modified
+// console.log(myArr);
+// console.log(myArr.splice(0,2));   /*--->>> Array Modified*/ 
+// console.log(myArr);
+
+console.log(myArr.sort());   // Sort the Array lexicographically
