e87cc0c3c495e65ae1ec276d0ca0f3bf39c6c77f
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2024-04-08 18:05:03 +0000

    Time Commit

--- Basics/07_DatesInJS.js
+++ Basics/07_DatesInJS.js
@@ -3,6 +3,8 @@ let myCreatedDate = new Date("01-14-2024");
 // console.log(myCreatedDate.toLocaleDateString());
 // console.log(myCreatedDate.toLocaleString());
 
+// In JS, Month Starts from 0;
+
 /***********************  Time  ********************* */
 
-
+console.log(myCreatedDate.getDay());
