432e7cd1fe31b4c985491d6c51fe7e0e52faa8d7
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2024-06-01 15:07:43 +0000

    till loops 1st

---
+++ 05_loops/forEach.js
@@ -0,0 +1,21 @@
+let arr = [5,8,9,7,6,2,10];
+
+// For normal functions 
+
+arr.forEach(function (val) {
+    // console.log(val);
+})
+
+// For arrow Function
+
+arr.forEach((hi)=>{
+    // console.log(hi);
+})
+
+// For already given function 
+
+function hi(item){
+    console.log(item);
+}
+
+arr.forEach(hi);
