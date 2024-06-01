bcde64e2429000edb520b165e1523f78a221d4e1
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2024-06-01 14:49:00 +0000

    till maps

---
+++ 05_loops/Map.js
@@ -0,0 +1,17 @@
+// const arr1 = [1,5,2,41,44];
+// for (const num of arr1) {
+//     console.log(num);
+// }
+
+ // Maps: Holds key value pair
+ 
+ let map = new Map();
+ map.set('H','Hello');
+ map.set('W','World');
+
+     for (const [key,value] of map) {     //[key,value]: used to print keys and values both at the same time 
+        console.log(key,value);
+ }
+
+ 
+
