2187cf710702d4763daabe256b6072ddd5f340c9
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2024-07-22 13:55:12 +0000

    Express js notes till response

--- notes/Express JS Materials/index.js
+++ notes/Express JS Materials/index.js
@@ -2,3 +2,9 @@ const express = require("express");
 const app = express();
 
 console.dir(app);
+
+let port = 3000;
+
+app.listen(port,() => {
+    console.log(`app is listening on the port ${port}`);
+})
