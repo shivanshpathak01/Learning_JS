babf64217ed6db69ee3fb2e5322f3eba31cb1ddb
Shivansh Pathak <shivanshpathak0001@gmail.com>
2024-09-02 21:59:08 +0530

    Till edit route : some tasks remaining

--- notes/Node with SQL/index.js
+++ notes/Node with SQL/index.js
@@ -1,6 +1,11 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require("express");
+const app = express();
+const path = require("path");
 
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"/views"));
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -17,24 +22,57 @@ const connection = mysql.createConnection({
     ];
   }
 
-
-  let q = "INSERT INTO user(id,username,email,password) VALUES ? ";
-
-  let data = [];
-  for(let i=1;i<=100;i++){
-    data.push(getRandomUser());
-    
-  }
-
-
+// Home Page
+app.get("/",(req,res)=>{
+  let q = `select count(*) from user`;
   try{
-  connection.query( q, [data], (err,result)=>{
-    if(err) throw err; 
-    console.log(result); 
-  }); 
-} catch(err){
-    console.log(err); 
-}
+    connection.query( q, (err,result)=>{
+      if(err) throw err; 
+      let count = result[0]["count(*)"];
+      res.render("home.ejs",{count}); 
+    }); 
+  } catch(err){
+      console.log(err);
+      res.send("Some error occurred"); 
+  } 
+});
+// Show Route 
+app.get("/user",(req,res)=>{
+  let q = `select * from user`;
+  try{
+    connection.query( q, (err,users)=>{
+      if(err) throw err; 
+      res.render("showusers.ejs",{users});
+      // res.send(result);
+      
+    }); 
+  } catch(err){
+      console.log(err);
+      res.send("Some error occurred 2 "); 
+  } 
+});
 
-connection.end();
+//Edit route 
+app.get("/user/:id/edit",(req,res)=>{
+  let {id} = req.params;
+  let q = `select * from user where id = '${id}'`;
+  res.render("edit.ejs");
+  try{
+    connection.query( q, (err,result)=>{
+      if(err) throw err; 
+      console.log(result);
+      res.render("edit.ejs");
+      // res.send(result);
+      
+    }); 
+  } catch(err){
+      console.log(err);
+      res.send("Some error occurred 2 "); 
+  } 
+});
 
+app.listen("8080",()=>{
+  console.log("App is listening to port");  
+})
+
+// connection.end();
