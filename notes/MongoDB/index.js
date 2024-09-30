686033eac730e2bc8d5918973caf4dc9f2c86751
Shivansh Pathak <shivanshpathak0001@gmail.com>
2024-09-30 11:36:50 +0530

    ...

---
+++ notes/MongoDB/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+main().
+    then(()=>{
+     console.log("Connection successful");
+    })
+    .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    age : Number
+});
+
+const user = mongoose.model("User",userSchema);
